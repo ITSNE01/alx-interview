bca650cee4dbaeb7c138fe673fc9d6f65271b25b
ITSNE01 <rabie.haddar1@gmail.com>
2025-04-22 10:25:02 +0000

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Determines if all locked boxes can be opened.
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list of lists): A list of indexes representing a box,
+        and each box contains a list of keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, or False.
+    """
+    if not isinstance(boxes, list):
+        return False
+
+    n = len(boxes)
+    opened = set([0])
+    keys = [0]
+
+    while keys:
+        current = keys.pop()
+        for key in boxes[current]:
+            if isinstance(key, int) and 0 <= key < n and key not in opened:
+                opened.add(key)
+                keys.append(key)
+
+    return len(opened) == n
